3e5dd3e583e756e166fe546dcedc3139313efbb9
ToolkitAdmin <Toolkit@protonmail.com>
2018-01-06 10:33:26 +0000

    General Upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CMS
+# OSINTToolkit.github.io
+
+Artemis A - Advanced (Admin & Development) User<br>
+Artemis B - Basic (Search Engine & Social Media) User<br>
+Artemis C - Full Access User<br>
+Artemis D - Custom Access User - Manual searching TR & PR modules<br>
+Artemis E - Custom Access User - Standalone pre-loaded module<br>
+Artemis F - Custom Access User - Manual searching module
---
+++ assets1/js/scroll2.js
@@ -0,0 +1,17 @@
+/*! jQuery v1.11.3 | (c) 2005, 2015 jQuery Foundation, Inc. | jquery.org/license */
+!
+$('body').prepend('<a href="#top" class="back-to-top">Back to Top</a>');
+var amountScrolled = 200;
+$(window).scroll(function() {
+if ( $(window).scrollTop() > amountScrolled ) {
+$('a.back-to-top').fadeIn('slow');
+} else {
+$('a.back-to-top').fadeOut('slow');
+}
+});
+$('a.back-to-top, a.simple-back-to-top').click(function() {
+$('html, body').animate({
+scrollTop: 0
+}, 700);
+return false;
+});
